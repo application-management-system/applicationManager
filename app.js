557c0b2ee9203a895d6047b47a8718fa000a7625
Sola <solaocodes@gmai.com>
2019-08-15 16:21:27 -0400

    Add post route to recieve form data

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/forms", formRouter);
+app.use("/form", formRouter);
 
 module.exports = app;
