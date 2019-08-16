4f7b0929512057bf962a4d0454dd67c96115066d
Sola <solaocodes@gmai.com>
2019-08-16 14:44:11 -0400

    Add some styling.

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var User = require("./models/user");
 
 var indexRouter = require("./routes/index");
 var formRouter = require("./routes/form");
 
+// connect to DB
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 var app = express();
 
 app.use("/static", express.static(path.resolve("../frontend/public")));
@@ -22,6 +32,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use("/", indexRouter);
 app.use("/form", formRouter);
